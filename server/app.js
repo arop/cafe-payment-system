a7e1329462d90e20af6c0d38b2b835353486d72c
Joao Norim Bandeira <norim_13@hotmail.com>
2016-10-12 19:20:08 +0100

    webservices proto.

--- server/app.js
+++ server/app.js
@@ -10,3 +10,51 @@ app.get('/', function (req, res) {
 
 app.listen(process.env.PORT || 5000);
 console.log('Server running in port ' + (process.env.PORT || 5000));
+
+
+
+///////////////////////////////////
+/////////    CUSTOMERS    /////////
+///////////////////////////////////
+//customer registration
+app.post('/register', function(req, res) {
+	//TODO
+});
+
+
+//customer validation
+app.post('/login', function(req, res) {
+	//TODO
+});
+
+
+
+///////////////////////////////////
+/////////    VOUCHERS     /////////
+///////////////////////////////////
+
+//voucher emition
+app.get('/voucher', function(req, res) {
+	//TODO
+});
+
+//voucher validation
+app.post('/voucher', function(req, res) {
+	//TODO
+});
+
+
+
+///////////////////////////////////
+/////////  TRANSACTIONS   /////////
+///////////////////////////////////
+
+//new transaction
+app.post('/transaction', function(req, res){
+	//TODO
+});
+
+//consult transactions
+app.get('/transaction', function(req, res){
+	//TODO
+});
