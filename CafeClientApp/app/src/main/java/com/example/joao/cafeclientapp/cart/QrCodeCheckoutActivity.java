ec56d80364224b99d121031fb345fc1592d0a86f
João Norim <norim_13@hotmail.com>
2016-10-21 02:32:13 +0100

    qr code generation.

---
+++ CafeClientApp/app/src/main/java/com/example/joao/cafeclientapp/cart/QrCodeCheckoutActivity.java
@@ -0,0 +1,64 @@
+package com.example.joao.cafeclientapp.cart;
+
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.example.joao.cafeclientapp.R;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+
+import static android.graphics.Color.BLACK;
+import static android.graphics.Color.WHITE;
+
+public class QrCodeCheckoutActivity extends AppCompatActivity {
+
+    private ImageView qrcodeView;
+    public final static int WIDTH = 400;
+    public final static int HEIGHT = 400;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr_code_checkout);
+
+        qrcodeView = (ImageView) findViewById(R.id.qrcode);
+        try {
+            Bitmap bitmap = encodeAsBitmap("VICUS");
+            qrcodeView.setImageBitmap(bitmap);
+            qrcodeView.invalidate();
+        } catch (WriterException e) {
+            e.printStackTrace();
+            int i = 0;
+        }
+
+    }
+
+
+    private Bitmap encodeAsBitmap(String str) throws WriterException {
+        BitMatrix result;
+        try {
+            result = new MultiFormatWriter().encode(str,
+                    BarcodeFormat.QR_CODE, WIDTH, WIDTH, null);
+        } catch (IllegalArgumentException iae) {
+            // Unsupported format
+            return null;
+        }
+        int w = result.getWidth();
+        int h = result.getHeight();
+        int[] pixels = new int[w * h];
+        for (int y = 0; y < h; y++) {
+            int offset = y * w;
+            for (int x = 0; x < w; x++) {
+                pixels[offset + x] = result.get(x, y) ? BLACK : WHITE;
+            }
+        }
+        Bitmap bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        bitmap.setPixels(pixels, 0, w, 0, 0, w, h);
+        return bitmap;
+    }
+}
